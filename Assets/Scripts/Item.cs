75f0820a96e7707384eaa0819630fbe9d81afd38
Martin Pišna <pisnamar@fel.cvut.cz>
2026-03-23 00:20:51 +0100

    added picking up and throwing item logic to player

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,45 +4,23 @@ public class Item : MonoBehaviour
 {
 
     private Rigidbody _rigidbody;
-    private PlayerInputActions _inputActions;
-
     public bool isBeingHeld = false;
 
     [SerializeField]
-    private float throwForce = 600.0f;
+    private float throwForce = 60.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _inputActions = new PlayerInputActions();
-        _inputActions.Enable();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_inputActions.Player.Interact.WasPressedThisFrame())
-        {
-            if (!isBeingHeld)
-            {
-                var attachPoint = GameObject.Find("Item Attach Point");
-                PickUp(attachPoint.transform);
-            }
-            else
-            {
-                Drop();
-            }
-        }
-        if(isBeingHeld && _inputActions.Player.Attack.WasPressedThisFrame())
-        {
-            var dir = GameObject.Find("MainCamera").transform.forward;
-            Throw(dir);
-        }
     }
 
-    void PickUp(Transform parent)
+    public void PickUp(Transform parent)
     {
         transform.parent = parent;
         transform.position = parent.position;
@@ -51,7 +29,7 @@ public class Item : MonoBehaviour
         isBeingHeld = true;
     }
 
-    void Drop()
+    public void Drop()
     {
         transform.parent = null;
         _rigidbody.isKinematic = false;
@@ -59,7 +37,7 @@ public class Item : MonoBehaviour
         isBeingHeld = false;
     }
 
-    void Throw(Vector3 direction)
+    public void Throw(Vector3 direction)
     {
         Drop();
         _rigidbody.AddForce(direction * throwForce, ForceMode.Impulse);
