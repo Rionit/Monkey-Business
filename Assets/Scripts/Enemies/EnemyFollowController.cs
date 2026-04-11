8fd8df833b3e0cc6fd0b27e37e60192cb3e9a30d
Martin Verner <martin.verner277@student.cuni.cz>
2026-04-12 01:36:28 +0200

    Added simple AI navigation

--- Assets/Scripts/Enemies/EnemyFollowController.cs
+++ Assets/Scripts/Enemies/EnemyFollowController.cs
@@ -1,7 +1,9 @@
+using System.Runtime.CompilerServices;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.AI;
 
+[assembly: InternalsVisibleTo("MonkeyBusiness.Tests")]
 namespace MonkeyBusiness.Enemies
 {
     public class EnemyMoveController : MonoBehaviour
@@ -11,9 +13,11 @@ namespace MonkeyBusiness.Enemies
         [Tooltip("NavMesh agent used for navigating")]
         NavMeshAgent _navMeshAgent;
 
-        [SerializeField]
-        [Tooltip("Target to chase")]
-        GameObject _chaseTarget;
+        [field: SerializeField]
+        [field: Tooltip("Target to chase")]
+        public GameObject ChaseTarget { get; private set; } 
+
+
 
         [SerializeField]
         [ReadOnly]
@@ -27,9 +31,11 @@ namespace MonkeyBusiness.Enemies
         [BoxGroup("Debug/Speed")]
         public float CurrentMaxSpeed
         {
-            get => _navMeshAgent.speed;
+            // Null check is only to avoid error in the Editor (at first, no NavMeshAgent is assigned). 
+            get => _navMeshAgent == null ? float.NaN : _navMeshAgent.speed;
             private set
             {
+                if (_navMeshAgent == null) return;
                 _navMeshAgent.speed = value;
             }
         }
@@ -46,9 +52,10 @@ namespace MonkeyBusiness.Enemies
         [BoxGroup("Debug/Speed")]
         public float CurrentAngularSpeed
         {
-            get => _navMeshAgent.angularSpeed;
+            get => _navMeshAgent == null ? float.NaN : _navMeshAgent.angularSpeed;
             private set
             {
+                if (_navMeshAgent == null) return;
                 _navMeshAgent.angularSpeed = value;
             }
         }
@@ -65,9 +72,10 @@ namespace MonkeyBusiness.Enemies
         [BoxGroup("Debug/Acceleration")]
         public float CurrentAcceleration
         {
-            get => _navMeshAgent.acceleration;
+            get => _navMeshAgent == null ? float.NaN : _navMeshAgent.acceleration;
             private set
             {
+                if (_navMeshAgent == null) return;
                 _navMeshAgent.acceleration = value;
             }
         }
@@ -84,9 +92,10 @@ namespace MonkeyBusiness.Enemies
         [BoxGroup("Debug")]
         public float CurrentStoppingDistance
         {
-            get => _navMeshAgent.stoppingDistance;
+            get => _navMeshAgent == null ? float.NaN : _navMeshAgent.stoppingDistance;
             private set
             {
+                if (_navMeshAgent == null) return;
                 _navMeshAgent.stoppingDistance = value;
             }
         }
@@ -97,9 +106,9 @@ namespace MonkeyBusiness.Enemies
         /// <remarks> <i> The actual avoidance priority will be a random value within this range.</i></remarks>
         [SerializeField]
         [MinMaxSlider(0, 100)]
-        [Tooltip("Avoidance priority range for the NavMeshAgent.<br/><br/> <i> The actual avoidance priority will be a random value within this range. </i>")]
+        [Tooltip("Avoidance priority range for the NavMeshAgent.\n\n<i>The actual avoidance priority will be a random value within this range. </i>")]
         Vector2Int _avoidancePriorityRange = new Vector2Int(20, 50);
-        
+
         [SerializeField]
         [Tooltip("Interval in seconds at which the enemy updates its path to the target.")]
         float _updatePathInterval = 1f;
@@ -120,7 +129,7 @@ namespace MonkeyBusiness.Enemies
 
         void Awake()
         {
-            _navMeshAgent.avoidancePriority = Random.Range(_avoidancePriorityRange.x, _avoidancePriorityRange.y);   
+            _navMeshAgent.avoidancePriority = Random.Range(_avoidancePriorityRange.x, _avoidancePriorityRange.y);
             _timeTillPathUpdate = _updatePathInterval;
             SetupDefaultValues();
         }
@@ -131,6 +140,11 @@ namespace MonkeyBusiness.Enemies
             _defaultMaxAngularSpeed = _navMeshAgent.angularSpeed;
             _defaultAcceleration = _navMeshAgent.acceleration;
             _defaultStoppingDistance = _navMeshAgent.stoppingDistance;
+        }
+
+        internal void TestSetup(float chaseDistance)
+        {
+            _chaseDistance = chaseDistance;
         }
 
         /// <summary>
@@ -146,18 +160,18 @@ namespace MonkeyBusiness.Enemies
 
         void UpdatePosition()
         {
-            if(_chaseTarget == null) return;
-            var distance = Vector3.Distance(transform.position, _chaseTarget.transform.position);
-            if(_runningAway && distance >= _chaseDistance * 1.1f)
+            if (ChaseTarget == null) return;
+            var distance = Vector3.Distance(transform.position, ChaseTarget.transform.position);
+            if (_runningAway && distance >= _chaseDistance * 1.1f)
             {
                 _runningAway = false;
             }
-            else if(!_runningAway && distance <= _chaseDistance * 0.9f)
+            else if (!_runningAway && distance <= _chaseDistance * 0.9f)
             {
                 _runningAway = true;
             }
 
-            _currentTargetPos = _runningAway ? GetRunawayPosition() : _chaseTarget.transform.position;
+            _currentTargetPos = _runningAway ? GetRunawayPosition() : ChaseTarget.transform.position;
             _navMeshAgent.SetDestination(_currentTargetPos);
         }
 
@@ -166,14 +180,14 @@ namespace MonkeyBusiness.Enemies
         /// </summary>
         Vector3 GetRunawayPosition()
         {
-            Vector3 directionToTarget = (_chaseTarget.transform.position - transform.position).normalized;
-            return _chaseTarget.transform.position - directionToTarget * _chaseDistance * 1.1f;
+            Vector3 directionToTarget = (ChaseTarget.transform.position - transform.position).normalized;
+            return ChaseTarget.transform.position - directionToTarget * _chaseDistance;
         }
 
         void FixedUpdate()
         {
             _timeTillPathUpdate -= Time.fixedDeltaTime;
-            if(_timeTillPathUpdate <= 0f)
+            if (_timeTillPathUpdate <= 0f)
             {
                 UpdatePosition();
                 _timeTillPathUpdate = _updatePathInterval;
@@ -184,7 +198,7 @@ namespace MonkeyBusiness.Enemies
         {
             Gizmos.color = _runningAway ? Color.green : Color.red;
             Gizmos.DrawWireSphere(transform.position, _chaseDistance);
-            if(_currentTargetPos != null )
+            if (_currentTargetPos != null)
             {
                 Gizmos.DrawLine(transform.position, _currentTargetPos);
             }
