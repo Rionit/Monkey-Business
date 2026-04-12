b7631068a8b44d3829d3f3051af85470d1108d35
Filip Doležal <fidokadan@gmail.com>
2026-04-12 23:58:09 +0200

    Added Advanced Health Bar

--- Assets/Scripts/UI/HealthBarController.cs
+++ Assets/Scripts/UI/HealthBarController.cs
@@ -1,5 +1,7 @@
 using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace MonkeyBusiness.UI
@@ -7,10 +9,40 @@ namespace MonkeyBusiness.UI
     public class HealthBarController : MonoBehaviour
     {
         
-        [SerializeField] private Image healthBar;
+        [SerializeField] private Image outline;
+        [SerializeField] private Image fill;
+        [SerializeField] private Image mask;
+
+        [PropertyRange(0f, "@endOffset")]
+        [SerializeField] private float startOffset = 100f;
+
+        [PropertyRange("@startOffset", "@GetImageWidth()")]
+        [SerializeField] private float endOffset = 100f;
         
         [Range(0f, 1f)]
         [SerializeField] private float value = 0.5f;
+
+        [BoxGroup("Health Bar Stuff")]
+        [HorizontalGroup("Health Bar Stuff/Colors", Title = "Colors"), HideLabel, LabelText("Low")]
+        [SerializeField] private Color lowHealthColor = Color.red;
+        [HorizontalGroup("Health Bar Stuff/Colors"), HideLabel, LabelText("Mid")]
+        [SerializeField] private Color mediumHealthColor = Color.darkOrange;
+        [HorizontalGroup("Health Bar Stuff/Colors"), HideLabel, LabelText("High")]
+        [SerializeField] private Color highHealthColor = Color.green;
+        
+        [HorizontalGroup("Health Bar Stuff/Sprites", Title = "Sprites"), HideLabel, LabelText("Low")]
+        [SerializeField] private Sprite lowHealthSprite;
+        [HorizontalGroup("Health Bar Stuff/Sprites"), HideLabel, LabelText("Mid")]
+        [SerializeField] private Sprite mediumHealthSprite;
+        [HorizontalGroup("Health Bar Stuff/Sprites"), HideLabel, LabelText("High")]
+        [SerializeField] private Sprite highHealthSprite;
+        
+        [HorizontalGroup("Health Bar Stuff/Masks", Title = "Masks"), HideLabel, LabelText("Low")]
+        [SerializeField] private Sprite lowHealthMaskSprite;
+        [HorizontalGroup("Health Bar Stuff/Masks"), HideLabel, LabelText("Mid")]
+        [SerializeField] private Sprite mediumHealthMaskSprite;
+        [HorizontalGroup("Health Bar Stuff/Masks"), HideLabel, LabelText("High")]
+        [SerializeField] private Sprite highHealthMaskSprite;
 
         private RectTransform rectTransform;
 
@@ -21,15 +53,52 @@ namespace MonkeyBusiness.UI
 
         private void OnValidate()
         {
-            if (healthBar != null)
+            if (fill != null)
             {
-                healthBar.rectTransform.sizeDelta = new Vector2(GetFillSize(value), healthBar.rectTransform.sizeDelta.y);
+                fill.rectTransform.sizeDelta = new Vector2(GetFillSize(value), fill.rectTransform.sizeDelta.y);
+                fill.color = GetFillColor(value);
+                SwitchSprites(value);
             }
+        }
+
+        private void SwitchSprites(float value)
+        {
+            if (outline == null || mask == null) return;
+            
+            if (value >= 0.75f)
+            {
+                outline.sprite = highHealthSprite;
+                mask.sprite = highHealthMaskSprite;
+            }
+            else if (value >= 0.25f)
+            {
+                outline.sprite = mediumHealthSprite;
+                mask.sprite = mediumHealthMaskSprite;
+            }
+            else
+            {
+                outline.sprite = lowHealthSprite;
+                mask.sprite = lowHealthMaskSprite;
+            }
+        }
+
+        private Color GetFillColor(float value)
+        {
+            if(value >= 0.75f) return highHealthColor;
+            else if(value >= 0.25f) return mediumHealthColor;
+            else return lowHealthColor;
         }
 
         private float GetFillSize(float value)
         {
-            return 50f + value * (GetComponent<RectTransform>().sizeDelta.x - 50f);
+            if (rectTransform == null) rectTransform = GetComponent<RectTransform>();
+            return Mathf.Lerp(startOffset, endOffset, value);
+        }
+        
+        private float GetImageWidth()
+        {
+            if (rectTransform == null) rectTransform = GetComponent<RectTransform>();
+            return rectTransform != null ? rectTransform.sizeDelta.x : 200f;
         }
     }
 }
