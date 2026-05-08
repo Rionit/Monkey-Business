fa77a7e60c12053e78f22102ba58e00c29fa9f8b
Martin Verner <martin.verner277@student.cuni.cz>
2026-05-08 20:46:02 +0200

    Tried implementing health-based pitch change

--- Assets/Scripts/Misc/MusicController.cs
+++ Assets/Scripts/Misc/MusicController.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using Ami.BroAudio;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Serialization;
+
 
 namespace MonkeyBusiness.Misc
 {
@@ -19,14 +22,33 @@ namespace MonkeyBusiness.Misc
         [SerializeField] private SoundSource perkLoopMusic;
         [SerializeField] private SoundSource perkTransitionSound;
 
+        [SerializeField,MinMaxSlider( 1f, 3f)] private Vector2 mainMusicPitchRange = new Vector2(1f, 1.3f);
+
         [Header("Tempo")]
         [SerializeField] private float bpm = 120f;
+
+        private float _lastBpm = DEFAULT_BPM;
+        
+        const float DEFAULT_BPM = 120f;
 
         [Header("Transition")]
         [SerializeField] private float perkTransitionDuration = 2.5f;
 
         private MusicState currentState = MusicState.None;
         private bool transitioning;
+
+        private Sequence _pitchTweenSequence;
+
+        private float _mainMusicPitch = 1f;
+        private float _MainMusicPitch
+        {
+            get => _mainMusicPitch;
+            set
+            {
+                _mainMusicPitch = value;
+                mainMusic.SetPitch(value);
+            }
+        }
 
         public void PlayMain()
         {
@@ -80,12 +102,15 @@ namespace MonkeyBusiness.Misc
                 yield break;
             }
 
+            bpm = _lastBpm; // Restore BPM to the last value before perk transition
+
             transitioning = false;
         }
 
         private IEnumerator PlayPerkRoutine()
         {
             transitioning = true;
+            _lastBpm = bpm;
 
             // MAIN -> PERK
             if (currentState == MusicState.Main)
@@ -102,6 +127,7 @@ namespace MonkeyBusiness.Misc
             }
 
             perkLoopMusic.Play();
+            bpm = DEFAULT_BPM; // Reset BPM to default for the transition
 
             currentState = MusicState.Perk;
             transitioning = false;
@@ -117,6 +143,30 @@ namespace MonkeyBusiness.Misc
 
             while (AudioSettings.dspTime < nextBeat)
                 yield return null;
+        }
+
+
+        // TODO: Fix so it changes pitch more than once
+        public void ChangePitch(float ratio)
+        {
+            Debug.Log("Changing pitch with ratio " + ratio);
+            if(_pitchTweenSequence != null && _pitchTweenSequence.active)
+            {
+                _pitchTweenSequence.Kill();
+            }
+
+            var newPitch = Mathf.Lerp(mainMusicPitchRange.x, mainMusicPitchRange.y, 1f - ratio);
+
+            Debug.Log("New pitch " + newPitch);
+            /*mainMusic.CurrentPlayer.AudioSource.pitchnewPitch, 0.5f);
+            bpm = DEFAULT_BPM * ratio;*/
+            _pitchTweenSequence = DOTween.Sequence();
+
+            mainMusic.CurrentPlayer.AudioSource.pitch = newPitch;
+
+            Debug.Assert(mainMusic.CurrentPlayer.AudioSource.pitch == newPitch, "Pitch was not set correctly on AudioSource!");
+            //mainMusic.SetPitch(newPitch, 0.5f);
+            bpm = DEFAULT_BPM * ratio;
         }
     }
 }
