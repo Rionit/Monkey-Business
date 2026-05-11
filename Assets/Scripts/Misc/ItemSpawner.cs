9b5c55f7d92b893eff97cc57688ae4c157db9dd9
Martin Pisna <pisnamar@cvut.cz>
2026-05-11 20:44:43 +0200

    Added item spawner prefab and replaced items in main scene with spawners

---
+++ Assets/Scripts/Misc/ItemSpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonkeyBusiness.Misc
+{
+    public class ItemSpawner : MonoBehaviour
+    {
+
+        [SerializeField]
+        private List<GameObject> _spawnableItems;
+
+        private GameObject _spawnedItem = null;
+
+        [SerializeField]
+        private float _spawnChance = .33f;
+
+
+        public void SpawnItem()
+        {
+            ClearItem();
+
+            if(Random.value < _spawnChance)
+            {
+                // Drop random item
+                _spawnedItem = Instantiate(_spawnableItems[Random.Range(0, _spawnableItems.Count)], transform.position, transform.rotation);
+            }
+        }
+
+        /// <summary>
+        /// Spawns a specific item. Can be used to override the default spawned item
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        public void SpawnItem(GameObject obj)
+        {
+            ClearItem();
+
+            if(Random.value < _spawnChance)
+            {
+                _spawnedItem = Instantiate(obj, transform.position, transform.rotation);
+            }
+        }
+
+
+        public void ClearItem()
+        {
+            if (!_spawnedItem)
+            {
+                return;
+            }
+
+            Destroy(_spawnedItem);
+            _spawnedItem = null;
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.limeGreen;
+            Gizmos.DrawWireCube(transform.position, Vector3.one * .25f);
+        }
+    }
+}
